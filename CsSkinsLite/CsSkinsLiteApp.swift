d1ac06004fe7b01166b73f7fd7606f19bccb1f23
brunothuma <bruno.thuma@hotmail.com>
2024-02-05 23:28:09 -0300

    Initial Commit

---
+++ CsSkinsLite/CsSkinsLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CsSkinsLiteApp.swift
+//  CsSkinsLite
+//
+//  Created by Bruno Thuma on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CsSkinsLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
